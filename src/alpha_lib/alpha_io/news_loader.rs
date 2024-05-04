219afcd8d4fd7cb822b4f46a51e379468e0ef415
dbrowne <dwight@dwightjbrowne.com>
2024-05-04 08:56:20 -0700

    News loader works with test data

--- src/alpha_lib/alpha_io/news_loader.rs
+++ src/alpha_lib/alpha_io/news_loader.rs
@@ -27,34 +27,37 @@
  * SOFTWARE.
  */
 
-
 extern crate chrono;
 
-use std::collections::{HashMap};
-use std::error::Error;
-use diesel::PgConnection;
 use crate::alpha_lib::alpha_io_funcs::{get_api_key, get_news_root};
-use crate::alpha_lib::news_type::{RawFeed, NewsRoot};
-use crate::dbfunctions::topic_refs::{get_topics, insert_topic};
+use crate::alpha_lib::news_type::{NewsRoot, RawFeed};
 use crate::create_url;
-use crate::dbfunctions::author::{get_author_by_id, get_authors, insert_author};
-use crate::dbfunctions::sources::{get_sources, insert_source};
-use chrono::prelude::*;
 use crate::dbfunctions::articles::insert_article;
+use crate::dbfunctions::author::{get_author_by_id, get_authors, insert_author};
 use crate::dbfunctions::news_root::insert_news_root;
+use crate::dbfunctions::sources::{get_sources, insert_source};
+use crate::dbfunctions::topic_refs::{get_topics, insert_topic};
 use crate::schema::articles::dsl::articles;
 use crate::schema::authors::author_name;
-
+use chrono::prelude::*;
+use diesel::PgConnection;
+use std::collections::HashMap;
+use std::error::Error;
 
 #[derive(Debug, Default)]
 pub struct Params {
-    pub  topics: HashMap<String, i32>,
-    pub  authors: HashMap<String, i32>,
-    pub  sources: HashMap<String, i32>,
-    pub  names_to_sid: HashMap<String, i64>,
+    pub topics: HashMap<String, i32>,
+    pub authors: HashMap<String, i32>,
+    pub sources: HashMap<String, i32>,
+    pub names_to_sid: HashMap<String, i64>,
 }
 
-pub fn load_news(conn: &mut PgConnection, s_id: &i64, tkr: &String, params: &mut Params) -> Result<(), Box<dyn Error>> {
+pub fn load_news(
+    conn: &mut PgConnection,
+    s_id: &i64,
+    tkr: &String,
+    params: &mut Params,
+) -> Result<(), Box<dyn Error>> {
     let api_key = get_api_key()?;
     let url = create_url!(FuncType:NewsQuery,tkr,api_key);
     let root = get_news_root(&url)?;
@@ -62,7 +65,13 @@ pub fn load_news(conn: &mut PgConnection, s_id: &i64, tkr: &String, params: &mut
     Ok(())
 }
 
-pub  fn process_news(conn: &mut PgConnection, s_id: &i64, tkr: &String, root: NewsRoot, params: &mut Params) -> Result<(), Box<dyn Error>> {
+pub fn process_news(
+    conn: &mut PgConnection,
+    s_id: &i64,
+    tkr: &String,
+    root: NewsRoot,
+    params: &mut Params,
+) -> Result<(), Box<dyn Error>> {
     let item_count = root.items.parse::<i32>()?;
     let sentiment_def = root.sentiment_score_definition;
     let relevance_def = root.relevance_score_definition;
@@ -77,48 +86,66 @@ pub  fn process_news(conn: &mut PgConnection, s_id: &i64, tkr: &String, root: Ne
     Ok(())
 }
 
-fn process_feed(conn: &mut PgConnection, s_id: &i64, tkr: &String, feed: Vec<RawFeed>, overview_id: i32, params: &mut Params) -> Result<(), Box<dyn Error>> {
+fn process_feed(
+    conn: &mut PgConnection,
+    s_id: &i64,
+    tkr: &String,
+    feed: Vec<RawFeed>,
+    overview_id: i32,
+    params: &mut Params,
+) -> Result<(), Box<dyn Error>> {
+    let mut ctr = 0;
     for article in feed {
+        ctr += 1;
+        if ctr > 44 {
+            println!("Hit the point!");
+        }
         process_article(conn, &s_id, &tkr, article, params)?;
     }
 
     Ok(())
 }
 
-
-fn process_article(conn: &mut PgConnection, s_id: &i64, tkr: &String, article: RawFeed, params: &mut Params) -> Result<(), Box<dyn Error>> {
+fn process_article(
+    conn: &mut PgConnection,
+    s_id: &i64,
+    tkr: &String,
+    article: RawFeed,
+    params: &mut Params,
+) -> Result<(), Box<dyn Error>> {
     let mut author_id: i32 = -1;
     let mut topic_id: i32 = -1;
     let mut source_id: i32 = -1;
 
-
     let sources = params.sources.clone();
-// bad logic here need to fix
-    if sources.is_empty(){
+    // bad logic here need to fix
+    if sources.is_empty() {
         let src = insert_source(conn, article.source.clone(), article.source_domain.clone())?;
         params.sources.insert(src.source_name, src.id.clone());
         source_id = src.id;
     } else {
-        for (src, _) in sources {
-            if params.sources.contains_key(&article.source.to_string()) {
-                source_id =  params.sources.get(&src.to_string()).unwrap().clone();
-                continue;
-            } else {
-                let src = insert_source(conn, article.source.clone(), article.source_domain.clone())?;
-                params.sources.insert(src.source_name, src.id.clone());
-                source_id = src.id;
-            }
+        if params.sources.contains_key(&article.source.to_string()) {
+            source_id = params.sources.get(&article.source.to_string()).unwrap().clone();
+        } else {
+            let src = insert_source(conn, article.source.clone(), article.source_domain.clone())?;
+            params.sources.insert(src.source_name, src.id.clone());
+            source_id = src.id;
         }
     }
 
     if (source_id == -1) {
-        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "No source id")));
+        return Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "No source id",
+        )));
     }
     let mut first_author_name = "_None_".to_string();
     let auths = params.authors.clone();
     if auths.is_empty() {
         let auth = insert_author(conn, article.authors[0].clone())?;
-        params.authors.insert(auth.author_name.clone(), auth.id.clone());
+        params
+            .authors
+            .insert(auth.author_name.clone(), auth.id.clone());
         author_id = auth.id;
     } else {
         if article.authors.len() > 0 {
@@ -130,31 +157,37 @@ fn process_article(conn: &mut PgConnection, s_id: &i64, tkr: &String, article: R
         } else {
             // Author doesn't exist in the map, insert it
             let auth = insert_author(conn, first_author_name)?;
-            params.authors.insert(auth.author_name.clone(), auth.id.clone());
+            params
+                .authors
+                .insert(auth.author_name.clone(), auth.id.clone());
             author_id = auth.id;
         }
     }
 
-    let art = insert_article(conn, source_id,
-                             article.category_within_source,
-                             article.title, article.url, article.summary,
-                             article.banner_image,
-                             author_id, article.time_published)?;
+    let art = insert_article(
+        conn,
+        source_id,
+        article.category_within_source,
+        article.title,
+        article.url,
+        article.summary,
+        article.banner_image,
+        author_id,
+        article.time_published,
+    )?;
 
     let authors = params.authors.clone();
 
-
-    for auth in &article.authors {
-        if params.authors.contains_key(auth) {
-            author_id = *params.authors.get(auth).unwrap_or(&-1);
-        } else {
-            println!("Inserting new author {}", article.authors[0].clone());
-            let auth = insert_author(conn, article.authors[0].clone())?;
-            params.authors.insert(auth.author_name, auth.id.clone());
-            author_id = auth.id;
-        }
-    }
-
+    // for auth in &article.authors {
+    //     if params.authors.contains_key(auth) {
+    //         author_id = *params.authors.get(auth).unwrap_or(&-1);
+    //     } else {
+    //         println!("Inserting new author {}", article.authors[0].clone());
+    //         let auth = insert_author(conn, article.authors[0].clone())?;
+    //         params.authors.insert(auth.author_name, auth.id.clone());
+    //         author_id = auth.id;
+    //     }
+    // }
 
     for topic in article.topics {
         if params.topics.contains_key(&topic.topic) {
@@ -165,8 +198,7 @@ fn process_article(conn: &mut PgConnection, s_id: &i64, tkr: &String, article: R
             let tp = insert_topic(conn, topic.topic)?;
             params.topics.insert(tp.name, tp.id.clone());
         }
-    };
-
+    }
 
     Ok(())
 }
